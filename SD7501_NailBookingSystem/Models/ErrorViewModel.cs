69d9ecd15336bb856763133d6ae9e7ff29fac35f
Andrew <Andrew@HEAVESPC>
2025-04-23 14:52:57 +1200

    Add project files.

---
+++ SD7501_NailBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD7501_NailBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
