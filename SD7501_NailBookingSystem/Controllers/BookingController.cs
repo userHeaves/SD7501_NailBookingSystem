29fada9ae46cc82cabf03c5fe4fe85ef54ec5a5f
RobertMcDougall <rtmcdougall@yahoo.co.nz>
2025-05-01 17:07:50 +1200

    Booking view done - Still need to work on CSS - Robert

---
+++ SD7501_NailBookingSystem/Controllers/BookingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SD7501_NailBookingSystem.Data;
+
+namespace SD7501_NailBookingSystem.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookingController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            var objBookingList = _db.Bookings.ToList();
+            return View(objBookingList);
+        }
+    }
+}
