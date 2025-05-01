1414e2b6935a751f830d0d2ddb85a887ec24e2a4
RobertMcDougall <rtmcdougall@yahoo.co.nz>
2025-05-01 16:07:58 +1200

    Set up connection string and downloaded necessary entity frameworks - Robert

---
+++ SD7501_NailBookingSystem/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SD7501_NailBookingSystem.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Booking Name")]
+        public string Name { get; set; }
+        [Range(1, 100)]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
