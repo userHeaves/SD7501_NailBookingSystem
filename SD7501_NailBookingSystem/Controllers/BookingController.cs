7fbfb48b54bd5cf11e4f0527eb6dcd4d52ba3662
RobertMcDougall <rtmcdougall@yahoo.co.nz>
2025-05-02 12:21:18 +1200

    Updating again

--- SD7501_NailBookingSystem/Controllers/BookingController.cs
+++ SD7501_NailBookingSystem/Controllers/BookingController.cs
@@ -7,6 +7,7 @@ namespace SD7501_NailBookingSystem.Controllers
     {
         private readonly ApplicationDbContext _db;
 
+        //Main View
         public BookingController(ApplicationDbContext db)
         {
             _db = db;
@@ -17,5 +18,96 @@ namespace SD7501_NailBookingSystem.Controllers
             var objBookingList = _db.Bookings.ToList();
             return View(objBookingList);
         }
+
+
+        //Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Booking bookingobj)
+        {
+            if (bookingobj.Name == bookingobj.BookingOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "This Display Order cannot exactly match with the name");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Bookings.Add(bookingobj);
+                _db.SaveChanges();
+                TempData["success"] = "Catergory created successfully";
+                return RedirectToAction("Index");
+            }
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Creation failed";
+            }
+            return View();
+        }
+
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Find the id
+            Booking? bookingFromDB = _db.Bookings.Find(id);
+
+            if (bookingFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(bookingFromDB);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Booking bookingobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Bookings.Update(bookingobj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Find the id
+            Booking? bookingFromDB = _db.Bookings.Find(id);
+
+            if (bookingFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(bookingFromDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Booking? bookingobj = _db.Bookings.Find(id);
+            if (bookingobj == null)
+            {
+                return NotFound();
+
+            }
+            _db.Bookings.Remove(bookingobj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
