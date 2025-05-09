3cb70b6fee309466ff3edd254fcc0bd0a770c198
RobertMcDougall <rtmcdougall@yahoo.co.nz>
2025-05-09 16:17:36 +1200

    Added CRUD to Service. Now all pages should be working. - Robert

--- SD7501_NailBookingSystem/Areas/Admin/Controllers/ServiceController.cs
+++ SD7501_NailBookingSystem/Areas/Admin/Controllers/ServiceController.cs
@@ -9,31 +9,130 @@ namespace SD7501_NailBookingSystem.Areas.Admin.Controllers
     [Area("Admin")] // Updated syntax for Area
     public class ServiceController : Controller
     {
-        private readonly IUnityOfWork _unitOfWork;//Updated syntax for UOF
-        public ServiceController(IUnityOfWork unityOfWork)//Updated syntax for UOF
+        private readonly IUnityOfWork _unitOfWork;
+        public ServiceController(IUnityOfWork unityOfWork)
         {
-            _unitOfWork = unityOfWork;//Updated syntax for UOF
+            _unitOfWork = unityOfWork;
         }
 
         public IActionResult Index()
         {
-            List<Service> objServiceList = _unitOfWork.Service.GetAll().ToList(); //Updated syntax for UOF
+            List<Service> objServiceList = _unitOfWork.Service.GetAll().ToList();
             return View(objServiceList);
         }
+
+        //Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Service serviceobj)
+        {
+            if (serviceobj.Type == serviceobj.Cost.ToString())
+            {
+                ModelState.AddModelError("Name", "This Display Order cannot exactly match with the name");
+
+            }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Service.Add(serviceobj);
+                _unitOfWork.Save();
+                TempData["success"] = "Service created successfully";
+                return RedirectToAction("Index");
+            }
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Service creation failed";
+            }
+            return View();
+        }
+
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Find the id
+            Service? serviceFromDB = _unitOfWork.Service.Get(u => u.Id == id);
+
+            if (serviceFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(serviceFromDB);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Service serviceobj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Service.Update(serviceobj);
+                _unitOfWork.Save();
+                TempData["success"] = "Service updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Edit failed";
+            }
+            return View();
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Find the id
+            Service? serviceFromDB = _unitOfWork.Service.Get(u => u.Id == id);
+
+            if (serviceFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(serviceFromDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Service? serviceobj = _unitOfWork.Service.Get(u => u.Id == id);
+            if (serviceobj == null)
+            {
+                return NotFound();
+
+            }
+            _unitOfWork.Service.Remove(serviceobj);
+            _unitOfWork.Save();
+            TempData["success"] = "Service deleted successful";
+            return RedirectToAction("Index");
+        }
+
         public class AddOnController : Controller
         {
-            private readonly IUnityOfWork _unitOfWork;//Updated syntax for UOF
-            public AddOnController(IUnityOfWork unityOfWork)//Updated syntax for UOF
+            private readonly IUnityOfWork _unitOfWork;
+            public AddOnController(IUnityOfWork unityOfWork)
             {
-                _unitOfWork = unityOfWork; //Updated syntax for UOF
+                _unitOfWork = unityOfWork;
             }
 
 
         }
-            public IActionResult Select()
-            {
-                List<AddOn> objAddOnList = _unitOfWork.AddOn.GetAll().ToList(); //Updated syntax for UOF
-                return View();
-            }
+        public IActionResult Select()
+        {
+            List<AddOn> objAddOnList = _unitOfWork.AddOn.GetAll().ToList();
+            return View();
+        }
     }
+
 }
