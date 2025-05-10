9dc941580323d3c295078e2e94dbe165db1b2941
RobertMcDougall <rtmcdougall@yahoo.co.nz>
2025-05-10 19:47:43 +1200

    Homepage for customer that shows the service. Still need to edit and change the details. - Robert

--- SD7501_NailBookingSystem/Areas/Customer/Controllers/HomeController.cs
+++ SD7501_NailBookingSystem/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using SD7501_NailBookingSystem.DataAccess.Repository.IRepository;
 using SD7501_NailBookingSystem.Models;
 
 namespace SD7501_NailBookingSystem.Areas.Customer.Controllers
@@ -8,15 +9,24 @@ namespace SD7501_NailBookingSystem.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnityOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnityOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Service> serviceList = _unitOfWork.Service.GetAll(includeProperties: "Booking");
+            return View(serviceList);
+        }
+
+        public IActionResult Details(int serviceId)
+        {
+            Service service = _unitOfWork.Service.Get(u => u.Id == serviceId, includeProperties: "Booking");
+            return View(service);
         }
 
         public IActionResult Privacy()
